561a813375a8638ea8c211a85c0fe6c327eaae76
hisayuki-mashimo <hisayuki.mashimo@befool.co.jp>
2018-07-14 20:42:18 +0900

    state

--- src/containers/Form/Main.tsx
+++ src/containers/Form/Main.tsx
@@ -1,20 +1,45 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
+import { connect } from "react-redux";
 
 class Form extends React.Component {
-  onlSelectChange() {
-    ReactDOM.findDOMNode(this.refs.form).submit();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sei: "",
+      mei: "",
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    const element = event.target;
+
+    this.setState({[element.name]: element.value});
+  }
+
+  onSelectChange() {
+    this.onSubmit();
   }
 
   onSubmit() {
-
+    this.props.dispatch({
+      type: "SET_USER",
+      user: {
+        sei: this.state.sei,
+        mei: this.state.mei,
+      },
+    });
   }
 
   render() {
     return <div id="form">
       <form ref="form" action="" method="get">
-        姓:<input type="text" name="sei" ref="sei" size="10" />
-        名:<input type="text" name="mei" ref="mei" size="10" />
+        姓:<input type="text" name="sei" onChange={this.onChange} size="10" />
+        名:<input type="text" name="mei" onChange={this.onChange} size="10" />
         <input type="button" onClick={this.onSubmit} value="診断" />
         <select ref="parameter_type"
           onChange={this.onSelectChange}
@@ -29,4 +54,4 @@ class Form extends React.Component {
   }
 }
 
-export default Form;
+export default connect()(Form);
